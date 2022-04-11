a7650641ebd86761510611c15777960e79f269cf
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2022-04-11 19:52:16 -0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+export default function Home(){
+  return (
+    <View style={{ 
+      flex: 1, 
+      justifyContent: 'center',
+      alignItems: 'center' 
+    }}>
+      <Text>Gabriel Carvalho</Text>
+      
+      <Text>Ignite</Text>
+    </View>
+  )
+}
