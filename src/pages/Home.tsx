3ec0bd882e503ccb3e721ffd35fdf0b5b22ea28a
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2022-04-13 17:14:14 -0300

    Refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,16 +7,32 @@ import {
   Platform,
   FlatList,
 } from 'react-native'
-import { Button } from './components/button';
-import { SkillCard } from './components/SkillCard';
+import { Button } from '../components/button';
+import { SkillCard } from '../components/SkillCard';
+
+type SkillData = {
+    id: string;
+    name: string;
+}
 
 export default function Home(){
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greetings, setGreetings] = useState('');
 
   function handleAddNewSkill(){
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id:string){
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ))
   }
 
   useEffect(() => {
@@ -49,7 +65,10 @@ export default function Home(){
         onChangeText={setNewSkill}
       />
 
-     <Button onPress={handleAddNewSkill} />
+     <Button 
+        onPress={handleAddNewSkill}
+        title="Adicionar"
+        />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>
         My Skills
@@ -58,9 +77,12 @@ export default function Home(){
 
       <FlatList 
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item} />
+          <SkillCard 
+            skill={item.name} 
+            onPress={() => handleRemoveSkill(item.id)}
+          />
         )}
       />
     </View>
@@ -70,7 +92,6 @@ const styles = StyleSheet.create({
   container: {
       flex: 1, 
       backgroundColor: '#161E2F',
-      paddingHorizontal: 20,
       paddingVertical: 70,
       paddingHorizontal: 30
   },
